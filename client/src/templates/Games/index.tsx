d9426b83cec8370f80ec61aaaf006313644c3dd9
Joao Silva <joao.devweb@gmail.com>
2022-02-01 16:18:48 -0300

    Changes from Section 36

--- client/src/templates/Games/index.tsx
+++ client/src/templates/Games/index.tsx
@@ -1,27 +1,30 @@
 import Base from 'templates/Base';
-import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown';
 
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar';
 import GameCard, { GameCardProps } from 'components/GameCard';
 import { Grid } from 'components/Grid';
 
+import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown';
+
 import * as S from './styles';
+import { useQueryGames } from 'graphql/queries/games';
 
 export type GamesTemplateProps = {
   games?: GameCardProps[];
   filterItems: ItemProps[];
 };
 
-export default function GamesTemplate({
-  filterItems,
-  games = [],
-}: GamesTemplateProps) {
+export default function GamesTemplate({ filterItems }: GamesTemplateProps) {
+  const { data, fetchMore, loading } = useQueryGames({
+    variables: { limit: 15 },
+  });
+
   const handleFilter = () => {
     return;
   };
 
   const handleShowMore = () => {
-    return;
+    fetchMore({ variables: { limit: 15, start: data?.games.length } });
   };
 
   return (
@@ -29,18 +32,29 @@ export default function GamesTemplate({
       <S.Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
 
-        <section>
-          <Grid>
-            {games.map((item) => (
-              <GameCard key={item.title} {...item} />
-            ))}
-          </Grid>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <section>
+            <Grid>
+              {data?.games.map((game) => (
+                <GameCard
+                  key={game.slug}
+                  title={game.name}
+                  slug={game.slug}
+                  developer={game.developers[0].name}
+                  img={`http://localhost:1337${game.cover!.url}`}
+                  price={game.price}
+                />
+              ))}
+            </Grid>
 
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <ArrowDown size={35} />
-          </S.ShowMore>
-        </section>
+            <S.ShowMore role="button" onClick={handleShowMore}>
+              <p>Show More</p>
+              <ArrowDown size={35} />
+            </S.ShowMore>
+          </section>
+        )}
       </S.Main>
     </Base>
   );
--- client/src/utils/apollo.ts
+++ client/src/utils/apollo.ts
@@ -1,38 +1,36 @@
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  NormalizedCacheObject,
-} from '@apollo/client';
+import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client';
 import { useMemo } from 'react';
+import apolloCache from './apolloCache';
 
-let apolloClient: ApolloClient<NormalizedCacheObject>;
+let apolloClient: ApolloClient<NormalizedCacheObject | null>;
 
 function createApolloClient() {
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
     link: new HttpLink({ uri: 'http://localhost:1337/graphql' }),
-    cache: new InMemoryCache(),
+    cache: apolloCache,
   });
 }
 
-export function initializeApollo(initialState = {}) {
-  // serve para verificar se já existe uma instância para não criar outra
+export function initializeApollo(initialState = null) {
+  // serve para verificar se já existe uma instância, para não criar outra
   const apolloClientGlobal = apolloClient ?? createApolloClient();
 
-  // recuperando os dados de cache
+  // se a página usar o apolloClient no lado client
+  // hidratamos o estado inicial aqui
   if (initialState) {
     apolloClientGlobal.cache.restore(initialState);
   }
 
   // sempre inicializando no SSR com cache limpo
   if (typeof window === 'undefined') return apolloClientGlobal;
+  // cria o apolloClient se estiver no client side
   apolloClient = apolloClient ?? apolloClientGlobal;
 
   return apolloClient;
 }
 
-export function useApollo(initialState = {}) {
+export function useApollo(initialState = null) {
   const store = useMemo(() => initializeApollo(initialState), [initialState]);
   return store;
 }
